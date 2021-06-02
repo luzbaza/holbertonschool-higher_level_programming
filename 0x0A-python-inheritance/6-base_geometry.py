af613a0b8421d17a18d176685c2968dce2ddbf4d
root <root@DESKTOP-VSC90TH.localdomain>
2021-06-01 23:35:25 -0500

    Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,9 +3,9 @@
 
 
 class BaseGeometry:
-     """Contains a function area()"""
+        """Contains a function area()"""
 
-     def area(self):
-        """Function not implemented yet"""
+        def area(self):
+            """Function not implemented yet"""
 
         raise Exception("area() is not implemented")
