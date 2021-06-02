ebbac0ce5e7f3ef344e7e0cef69849bcfe5c6663
root <root@DESKTOP-VSC90TH.localdomain>
2021-06-02 17:26:27 -0500

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" read_file """
+
+
+def read_file(filename=""):
+    """ function that reads a text file """
+
+    with open(filename, "r", encoding="utf-8") as readfile:
+        print(readfile.read(), end="\n")
