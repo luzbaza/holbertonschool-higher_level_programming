2b3c583fc0faf989ccd88a442dab2c60ad04459f
root <root@DESKTOP-VSC90TH.localdomain>
2021-06-08 16:10:12 -0500

    files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Base"""
+
+
+class Base:
+    """ Base of the other shapes """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
