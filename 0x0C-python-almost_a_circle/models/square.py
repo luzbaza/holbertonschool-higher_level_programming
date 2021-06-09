6f66d7dd015ada79059c60f2025028a1242789c5
root <root@DESKTOP-VSC90TH.localdomain>
2021-06-09 00:15:51 -0500

    files

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" square """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Inherits from Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ method so that it returns Rectangle """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.size)
