0a6f2f90cb0e8af200dba0cc90bb97dbb31b207d
luzbaza <2459@holbertonschool.com>
2021-07-13 16:28:48 -0500

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- MYSQL
 --  script that creates the MySQL server use
-CREATE DATABASE IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
