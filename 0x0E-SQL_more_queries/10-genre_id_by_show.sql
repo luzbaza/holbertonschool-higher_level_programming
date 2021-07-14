4a5e7d41edc2d43fa13bbb0ed5dcbc8c731ec413
luzbaza <2459@holbertonschool.com>
2021-07-13 23:09:51 -0500

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 -- Import the database dump from hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_show_genres.show.id = tv_shows.id
+RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
