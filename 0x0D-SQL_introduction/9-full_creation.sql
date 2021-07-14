61254e57724020a8704d2fe46bd99d15b24d4358
luzbaza <2459@holbertonschool.com>
2021-07-14 16:23:48 -0500

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,3 @@
 --script that creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table(
-	id INT DEFAULT NULL,
-	name VARCHAR(256),
-	score INT
-);
--- script that insert a table second_table
-INSERT INTO 'second_table'('id', 'name', 'score')
-VALUES
-		(1, "John", 10),
-		(2, "Alex", 3),
-		(3, "Bob", 14),
-		(4, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
