f7520501d27bacdf890fecf47f1de5effd5f08d8
luzbaza <2459@holbertonschool.com>
2021-07-13 22:37:53 -0500

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- MYSQL
+-- script that lists all the cities of California that can be found 
+-- in the database hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.states_id = states.id
+	AND states.name = 'California'
+ORDER BY cities.id ASC;
