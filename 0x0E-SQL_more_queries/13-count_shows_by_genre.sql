f3650360c3ff1c219300da0a1a5daae439c3bc25
luzbaza <2459@holbertonschool.com>
2021-07-13 23:33:25 -0500

    Number of shows b

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- MYSQL
+-- Import the database dump from hbtn_0d_tvshows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
