f7cceae1c58ebddf5f452db54d0398206eae9ebe
luzbaza <2459@holbertonschool.com>
2021-07-14 22:23:59 -0500

    Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Import in hbtn_0c_0 database
-SELECT city, AVG(value) as avg_temp FROM temperatures WHERE mont IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
