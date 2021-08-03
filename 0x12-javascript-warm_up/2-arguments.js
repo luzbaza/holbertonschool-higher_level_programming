9f20a08bc4bdfd155e98c4838f44caf311238920
luzbaza <2459@holbertonschool.com>
2021-08-02 22:00:08 -0500

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* script that prints a message depending of the number of arguments passed */
+/*script that prints a message depending of the number of arguments passed*/
 
 if (process.argv.length === 2) {
   console.log('No argument');
