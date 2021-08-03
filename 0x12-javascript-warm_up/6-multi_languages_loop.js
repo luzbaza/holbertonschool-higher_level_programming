a76526f7b3479e2933dc35331ad67a54c6079ca3
luzbaza <2459@holbertonschool.com>
2021-08-02 23:27:43 -0500

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* script that prints 3 lines */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
