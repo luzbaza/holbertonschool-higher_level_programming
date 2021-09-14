8f34bd1fc5f1db663f091c3785b56b70a1a2157e
luzbaza <2459@holbertonschool.com>
2021-09-14 17:29:48 -0500

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//  script that writes a string to a file
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], process.argv[3], 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+})
+;
