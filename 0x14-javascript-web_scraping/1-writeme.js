b87d570ed21533c59030d7cad0995dc111d49c74
luzbaza <2459@holbertonschool.com>
2021-09-14 17:35:00 -0500

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], process.argv[3], 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) {
     console.error(err);
   }
