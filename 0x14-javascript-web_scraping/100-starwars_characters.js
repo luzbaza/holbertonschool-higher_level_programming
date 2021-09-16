d15143d43efd57cac3bd7edd63a12ffbfb7b4c6b
luzbaza <2459@holbertonschool.com>
2021-09-15 22:22:09 -0500

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+//  script that prints all characters of a Star Wars movie
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+let movieDict = {};
+let characterDict = {};
+let i = 0;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    movieDict = JSON.parse(body);
+    for (i = 0; i < movieDict.characters.length; i++) {
+      request(movieDict.characters[i], function (error, request, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          characterDict = JSON.parse(body);
+          console.log(characterDict.name);
+        }
+      });
+    }
+  }
+});
