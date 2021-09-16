e1659886d5d36acfdaa3b9138b2d3fbcc8eb7f0d
luzbaza <2459@holbertonschool.com>
2021-09-15 23:07:51 -0500

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+//  script that prints all characters of a Star Wars movie
+
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const role = JSON.parse(body).characters;
+    print(role, 0);
+  }
+});
+
+function print (array, i) {
+  if (i >= array.length) {
+    return;
+  }
+  request.get(array[i], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
+    return print(array, i + 1);
+  });
+}
