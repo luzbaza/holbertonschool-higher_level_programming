7486bf9d41491d27d92a32ef3fe5c7cc2e12f5a0
= <=>
2023-02-01 10:45:51 -0500

    JavaScript - Warm up

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-/* script that prints two arguments passed to it */
-
-console.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-/* first argument can be converted to an integer */
-
-if (!parseInt(process.argv[2])) { // parseInt: conviert de str to int
-  console.log('Not a number');
-} else {
-  console.log('My number: %d', parseInt(process.argv[2]));
-}
