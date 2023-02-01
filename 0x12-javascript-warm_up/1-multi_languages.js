7775f1c426c8ce1e621eea5aac8b321f7d5a5597
= <=>
2023-02-01 11:39:53 -0500

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let lenguages = 'C is fun \n\
-Python is cool \n\
-JavaScript is amazing \n';
+const lenguages = 'C is fun \n' +
+'python is cool \n' +
+'JavaScript is amazing \n';
 
 console.log(lenguages);
